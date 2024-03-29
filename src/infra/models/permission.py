5ae3639a6f310e4d70e5c1609340fd179f6ae828
Ilya Shevelev <ilasevelev10776@gmail.com>
2024-03-29 09:39:04 +0300

    Added User, Permission, Role models

---
+++ src/infra/models/permission.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from src.lib.models import Base
+
+
+class PermissionModel(Base):
+    """
+    Модель разрешения
+
+    :param code: Кодовое название разрешения
+    """
+
+    __tablename__ = "permissions"
+
+    code: Mapped[str] = mapped_column(String(20), unique=True)
+    description: Mapped[str] = mapped_column(String(50), nullable=True)
+    roles: Mapped[list["RoleModel"]] = relationship("RoleModel", secondary="roles_permissions", back_populates="permissions")
---
+++ src/infra/models/role_permission.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped
+
+from src.lib.models import Base
+
+
+class RolePermissionModel(Base):
+    """
+    Смежная таблица ролей и разрешений
+
+    :param role_id: ID роли
+    :param permission_id: ID разрешения
+    """
+
+    __tablename__ = "roles_permissions"
+
+    role_id: Mapped[UUID] = mapped_column(ForeignKey("roles.id", ondelete="CASCADE"), primary_key=True)
+    permission_id: Mapped[UUID] = mapped_column(ForeignKey("permissions.id", ondelete="CASCADE"), primary_key=True)
